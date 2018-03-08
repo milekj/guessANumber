9be97133e8070d2e9d58c60d13b65cd7168f9980
milekj <m.i.l.e.k.j.a.n@gmail.com>
2018-03-08 14:10:29 +0100

    ...

---
+++ guessFun.py
@@ -0,0 +1,38 @@
+import random
+
+def randInRange ():
+    while True:
+        s1 = input("Podaj dwie liczby:\n")
+        s2 = input()
+
+        try:
+            n1 = int(s1)
+            n2 = int(s2)
+
+        except ValueError:
+            print("To nie sa prawidlowe liczby")
+            continue
+            
+        if n1 > n2:
+            print("Bledny przedzial")
+        else:
+            return random.randint(n1, n2)
+
+def guess (number):
+        while True:
+            s = input("Podaj liczbe:\n")
+
+            try:
+                n = int(s)
+
+            except ValueError:
+                print("To nie jest prawidlowa liczba")
+                continue
+            
+            if number == n:
+                print("Brawo! Magiczna liczba to: ", number)
+                return
+            else:
+                print("Nie udalo sie! Probuj dalej...")
+
+guess(randInRange())
